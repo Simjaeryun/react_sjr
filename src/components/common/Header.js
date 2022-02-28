43628d9d6b06b848ecc8553e66f0f495b18de19e
Simjaeryun <simjaeding@gmail.com>
2022-02-28 18:49:46 +0900

    header logo추가

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,8 +1,9 @@
 export default function Header() {
+    const path = process.env.PUBLIC_URL;
     return (
         <header className="header">
             <div className="header_logo">
-                IMAGE
+                <img src={`${path}/img/bear_logo.gif`} alt="" />
             </div>
         </header>
     )
